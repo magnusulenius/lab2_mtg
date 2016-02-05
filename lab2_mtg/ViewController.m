76245399b9afa09061a60bf33d03eff137b313cf
aguelo <axel.jonsson@live.com>
2016-02-05 13:58:03 +0100

    init

---
+++ lab2_mtg/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lab2_mtg
+//
+//  Created by Axel Jonsson on 04/02/16.
+//  Copyright © 2016 JAM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
