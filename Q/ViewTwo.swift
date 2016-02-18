95db92c4b4c5172c170f125710fcbe31399a6275
aguelo <axel.jonsson@live.com>
2016-02-18 18:56:05 +0100

    Added segue after login

--- Q/ViewTwo.swift
+++ Q/ViewTwo.swift
@@ -83,12 +83,15 @@ class ViewTwo: UIViewController, SPTAudioStreamingPlaybackDelegate {
 
     @IBAction func logoutSpotify(sender: AnyObject) {
 
-        print("logout")
         self.player.logout({ (error : NSError!) -> Void in
             if error != nil {
                 print("Logout error")
                 return
             }
+            SPTAuth.defaultInstance().session = nil
+            let userDefaults = NSUserDefaults.standardUserDefaults()
+            userDefaults.synchronize()
+            print("logout complete")
         })
     }
     
